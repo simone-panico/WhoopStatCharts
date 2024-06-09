deba5f9c76250b820a2207cfea7dd6840f6e5f33
Simone Panico <simone.panico@icloud.com>
2024-06-09 16:47:16 +0200

    Initial Commit

---
+++ WhoopAPI/WhoopAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoopAPIApp.swift
+//  WhoopAPI
+//
+//  Created by Simone Panico on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhoopAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
