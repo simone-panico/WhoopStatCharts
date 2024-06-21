d5f445ecf723ea4042011f4a08c8cc5f3ee06f1c
Simone Panico <simone.panico@icloud.com>
2024-06-21 22:16:29 +0200

    First Graph with Example data

--- WhoopAPI/ContentView.swift
+++ WhoopAPI/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import Charts
+
+struct workoutStrain: Identifiable {
+    var id = UUID()
+    var strain: Float
+    var created_at: Date
+    
+    init(strain: Float, created_at: String) {
+        let inputDateFormatter = DateFormatter()
+        inputDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        inputDateFormatter.locale = Locale(identifier: "en_US_POSIX")
+
+        let date = inputDateFormatter.date(from: created_at)
+        
+        self.created_at = date ?? Date()
+        self.strain = strain
+    }
+}
+
+
+
+var data: [workoutStrain] = [
+    workoutStrain(strain: 13.5321, created_at: "2024-06-16T09:54:04.559Z"),
+    workoutStrain(strain: 16.1525, created_at: "2024-06-13T14:37:40.695Z"),
+    workoutStrain(strain: 11.2071, created_at:  "2024-06-11T13:22:20.779Z"),
+    workoutStrain(strain: 12.8494, created_at: "2024-06-09T08:58:51.116Z"),
+    workoutStrain(strain: 9.395, created_at:  "2024-06-07T16:05:44.105Z"),
+    workoutStrain(strain: 15.4325, created_at: "2024-06-06T15:50:04.398Z")
+]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Chart(data) {
+            LineMark(x: .value("Date", $0.created_at), y: .value("Strain", $0.strain))
         }
-        .padding()
     }
 }
 
@@ -23,6 +48,5 @@ struct ContentView: View {
     ContentView()
 }
 
-struct workoutStrain {
-    var strain: Float
-}
+
+
